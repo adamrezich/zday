70f17290d88c794beb04aaa0fcae0e2f5817c119
Adam Rezich <adam@rezich.com>
2012-01-16 00:35:33 -0700

    console is now functional

---
+++ ZDay/Console.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZDay {
+	class Console {
+		public static List<ConsoleLine> Lines = new List<ConsoleLine>();
+
+		public static void WriteLine(string text) {
+			Lines.Add(new ConsoleLine(text));
+		}
+	}
+
+	class ConsoleLine {
+		public string Text;
+
+		public ConsoleLine(string text) {
+			Text = text;
+		}
+	}
+}
--- ZDay/Game.cs
+++ ZDay/Game.cs
@@ -17,6 +17,9 @@ namespace ZDay {
 		}
 
 		public void Play() {
+			Console.WriteLine("Welcome to Z-Day!");
+			Console.WriteLine("Testing a really long line here that is so damn long, it's going to have to be wrapped.");
+			Console.WriteLine("...Again, welcome to Z-Day!");
 			while (!Over && !TCODConsole.isWindowClosed()) {
 				Draw();
 				Update();
@@ -63,6 +66,9 @@ namespace ZDay {
 					player.Position.X++;
 					player.Position.Y++;
 					break;
+				case TCODKeyCode.Space:
+					Console.WriteLine("TEST " + Convert.ToString(Console.Lines.Count));
+					break;
 
 			}
 		}
@@ -78,14 +84,17 @@ namespace ZDay {
 			int weaponBoxHeight = 5;
 			int characterBoxHeight = 11;
 
+
+			// bottom bar
 			r.printFrame(0, windowHeight - 3, windowWidth - sidebarWidth, 3);
 
+
+			// character box
 			r.printFrame(vWidth, 0, windowWidth - vWidth, characterBoxHeight);
 			r.print(vWidth + 2, 0, "CHARACTER");
 			r.print(vWidth + 2, 2, "Adam");
 			r.print(vWidth + 2, 3, "LVL: 1");
 			r.print(vWidth + 2, 4, "SPD: 4");
-
 
 			float barHP = ((float)8 / (float)10) * (windowWidth - vWidth - 4);
 			float barStamina = ((float)67 / (float)100) * (windowWidth - vWidth - 4);
@@ -112,11 +121,40 @@ namespace ZDay {
 			r.print(vWidth + 24, 3, "INT: 10");
 			r.print(vWidth + 16, 4, "ATK: 2d8 + 4");
 
+
+			// console box
 			r.printFrame(vWidth, characterBoxHeight, windowWidth - vWidth, windowHeight - weaponBoxHeight - characterBoxHeight);
 			r.print(vWidth + 2, characterBoxHeight, "CONSOLE");
+			int remainingLines = windowHeight - weaponBoxHeight - characterBoxHeight - 2;
+			int i = Console.Lines.Count - 1;
+			while (remainingLines > 0 && i > -1) {
+				string text = Console.Lines[i].Text;
+				Queue<string> lines = new Queue<string>();
+				int maxLength = windowWidth - vWidth - 2;
+				while (text.Length > maxLength) {
+					lines.Enqueue(text.Substring(0, maxLength));
+					text = " " + text.Substring(maxLength).Trim();
+				}
+				lines.Enqueue(text);
+				remainingLines -= lines.Count;
+				int j = 0;
+				while (lines.Count > 0) {
+					string line = lines.Dequeue();
+					if (characterBoxHeight + 1 + remainingLines + j > characterBoxHeight) {
+						r.print(vWidth + 1, characterBoxHeight + 1 + remainingLines + j, line);
+					}
+					j++;
+				}
+				
+				i -= 1;
+			}
 
+
+			// weapon box
 			r.printFrame(vWidth, 45, weaponBoxWidth, weaponBoxHeight);
 
+
+			// time box
 			r.printFrame(vWidth + weaponBoxWidth, 45, windowWidth - vWidth - weaponBoxWidth, weaponBoxHeight);
 			string strKilled = "10000 KILLED";
 			string strTime = "DAY 1 00:00.00";
